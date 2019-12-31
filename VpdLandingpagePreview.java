816c349f68e532294b5579fe7dff0f74297deb99
sukhendu555 <sukhendutiny@gmail.com>
2019-12-30 21:53:47 -0600

    Add files via upload

--- VpdLandingpagePreview.java
+++ VpdLandingpagePreview.java
@@ -12,31 +12,29 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.asserts.SoftAssert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 
 import com.vzw.ccpa.test.Datattest;
 import com.vzw.pos.automation.drivers.BasePage;
 
 public class VpdLandingpagePreview extends BasePage {
 
-
-
-	private WebDriver driver;     //div[@id='accordion__heading-accountinfo']
-	@FindBy(xpath="//*[@id='accordion__heading-accountinfo']/span[1]")
-	WebElement clickon_previewplus;
-	//@FindBy(xpath="")
-	//WebElement personalinfo
-
-	@FindBy(xpath="//*[@id='accordion__heading-4']")
-	WebElement clickion_personalinfo;
-
-	@FindBy(css="#accordion__panel-accountinfo > div")
-	WebElement Privwedata;
-	@FindBy(xpath="//*[@id='accordion__heading-5']")
-	WebElement Government_identifiers;
-	@FindBy(xpath="//*[@id='accordion__heading-6']")
-	WebElement BillingNfinincial;
-	@FindBy(xpath="//*[@id='accordion__heading-7']")
-	WebElement TechnicalIdentifier;
+	WebDriver driver;
+	WebDriverWait wait;
+	JavascriptExecutor jsx;
+	
+	//Categories
+	By AccountAndIdentity = By.xpath("//*[@id=\"accordion__heading-accountinfo\"]");
+	By NetworkAndCustomerActivity = By.xpath("//*[@id=\"accordion__heading-networkactivity\"]");
+	By ThirdPartyInsights = By.xpath("//*[@id=\"accordion__heading-collectedinferences\"]");
+	By AddOns = By.xpath("//*[@id=\"accordion__heading-addons\"]");
+	
+	By ExpandedAccountAndIdentity = By.xpath("//*[@id=\"accordion__panel-accountinfo\"]");
+	By ExpandedNetworkAndCustomerActivity = By.xpath("//*[@id=\"accordion__panel-networkactivity\"]");
+	By ExpandedThirdPartyInsights = By.xpath("//*[@id=\"accordion__panel-collectedinferences\"]");
+	By ExpandedAddOns = By.xpath("//*[@id=\"accordion__panel-addons\"]");;
+	
 
 	Hashtable<String, String> hTable;
 
@@ -46,120 +44,122 @@ public class VpdLandingpagePreview extends BasePage {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 		this.hTable=table;
-
-
-
 	}
 
 	//vPdLand("clickon_previewplus","clickion_personalinfo");
 	@SuppressWarnings("unchecked")
 	public void vPdLand() throws InterruptedException, IOException {
-		//captureScreenshotPage((Map<String, String>) driver,"page");
-		//clickonElement(driver, Clickptr1priview, "", SAPB2BArea);
-		//try {
-		subCatList=new ArrayList<String>();
-		subCatList.add("Contact Information and Personal Identifiers");
-		subCatList.add("Government identifiers");
-		subCatList.add("Billing and Financial Information");
-		subCatList.add("Technical identifiers");
+		int timeOutInSeconds = 120; //2 minutes
+		wait = new WebDriverWait(driver, timeOutInSeconds);
+		jsx = (JavascriptExecutor) driver;
 		
+		List<String> subCategoryAccountAndIdentity = new ArrayList<String>();
+		subCategoryAccountAndIdentity.add("Contact Information and Personal Identifiers");
+		subCategoryAccountAndIdentity.add("Government identifiers");
+		subCategoryAccountAndIdentity.add("Line Information");
+		
+		List<String> subCategoryAddOns = new ArrayList<String>();
+		subCategoryAddOns.add("Verizon Cloud");
 
-		SoftAssert softAsseRt = new SoftAssert();
-			
-
+//		jsx.executeScript("window.scrollBy(0,100)");
+		//Expand Account and Identity Category
+		wait.until(ExpectedConditions.visibilityOfElementLocated(AccountAndIdentity)).click();
 		try {
-
-			String str00=driver.findElement(By.xpath("//*[@id='root']/div[3]/div/div[2]/div/h2[1]")).getText();
-			softAsseRt.assertEquals("Your personal information", str00);
-
-			System.out.println("Pass: Your personal informagiton is exist in vpd landing page"+str00);
-
-			System.out.println("Pass: Preview dashboard page your personla information text exist");
-			
-            Thread.sleep(7000);
-			WebElement scrol = driver.findElement(By.xpath("//*[contains(@id,'accordion__heading-accountinfo')]"));
-			((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrol);
-			//clickonElement(driver,personalinfo,"pass","fail");
-			
-			Thread.sleep(7000);
-			clickonElement(driver,clickon_previewplus,"Pass: user able to click on accountaidentityplus sign","Fail user unale to click on accountaidentityplus sign");
-			Thread.sleep(7000);
-			clickonElement(driver,clickion_personalinfo,"Pass: user able to click on accountidentityplus sign","Fail: user not able to click on clickion_personalinfo");
-		
-		 
-			//WebElement scrol2 = driver.findElement(By.xpath("//span[contains(text(),'Government identifiers')]"));
-			//  ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrol2);
-			try {
-				clickElement(driver,Government_identifiers,"Pass: user able to clcick on Government identifiers subcategories","Fail:user Unable to clcick on Government identifiers subcategories ");
-			}catch(Exception exp) {
-				System.out.println("We don't have this sub cat..!!");
-			}
-			//WebElement check = driver.findElement(By.xpath("//*[@id='accordion__heading-5"));
-			//softAsseRt.assertNull(check);
-			// scrol2 = driver.findElement(By.xpath("//span[contains(text(),'Billing and Financial Information')]"));
-			//  ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrol2);
-			JavascriptExecutor js = (JavascriptExecutor) driver;
-			js.executeScript("arguments[0].scrollIntoViewIfNeeded(true);", js);
-				clickonElement(driver,BillingNfinincial,"Pass: user able to clcick on BillingNfinincial subcategories","Fail:user Unable to clcick on BillingNfinincial subcategories ");
-			}
-			catch(Exception exp) {
-				System.out.println("We don't have this sub cat..!!");
-			}
-			// scrol2 = driver.findElement(By.xpath("//span[contains(text(),'Government identifiers')]"));
-			//((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", scrol2);
-			try {
-				clickonElement(driver,TechnicalIdentifier,"Pass: user able to clcick on TechnicalIdentifier subcategories","Fail:user Unable to clcick on TechnicalIdentifier subcategories ");
-			}catch(Exception exp) {
-				System.out.println("We don't have this sub cat..!!");
-			}
-			try {
-			//String str = driver.findElement(By.cssSelector("#accordion__panel-4")).getText();
-			List<WebElement> elements = driver.findElements(By.xpath("//span[@class='sc-gFaPwZ bMusfh']"));//this will return value 
-			List<WebElement> elements1 = driver.findElements(By.xpath("//p[@class='sc-jzgbtB jSkAjc']"));//this will return value 
-			
-			int i1=0;
-			//String str1,str2,str3,str4,str5,str6;
-			String s1="MTN";
-			for (WebElement element : elements) {
-				if(!element.getText().equalsIgnoreCase("") && !subCatList.contains(element.getText())) {
-					s1=s1+","+element.getText();
-					System.out.println("|"+element.getText()+"|");
-				}
-				i1=i1+1;
-			}	
-			Datattest.createHeader(s1);
-
-			i1=0;
-			//String str1,str2,str3,str4,str5,str6;
-			String s=hTable.get("MTN");
-			for (WebElement element : elements1) {
-				
-				s=s+","+element.getText();
-				
-				System.out.println(element.getText());
-				i1=i1+1;
-			}	
-			Datattest.updateResultsSummary2(s);	
-
-			//softAsseRt.assertEquals("Pass:You can update your contact information in My Verizon. message exist",str7);
-			softAsseRt.assertAll();
-			
-			}
-			catch(Exception e1) {
-				e1.printStackTrace();
-
-
+			wait.until(ExpectedConditions.visibilityOfElementLocated(ExpandedAccountAndIdentity));
+			System.out.println("Test Pass: Account and Identity Category expanded");
+		}catch(Exception e) {
+			Assert.fail("Test Fail: Account and Identity Category does not expand");
 		}
 		
-			 
-			 
+		//Assert Sub-category titles of Account and Identity Category
+		for (int i=0; i<subCategoryAccountAndIdentity.size();i++) {
+			String subCategory = subCategoryAccountAndIdentity.get(i);
+			By xpath = SubCategoryXpath(subCategory);
+			try {
+				wait.until(ExpectedConditions.visibilityOfElementLocated(xpath));
+				System.out.println("Test Pass: Sub Category \"" + subCategory + "\" is available");
+			}catch (Exception e) {
+				Assert.fail("Test Fail: Sub Category \""+ subCategory + "\" is not available");
+			}
+		}
 		
+		//Expand Sub-category of Account and Identity Category
+		for (int i=0; i<subCategoryAccountAndIdentity.size();i++) {
+			String subCategory = subCategoryAccountAndIdentity.get(i);
+			By xpath = SubCategoryXpath(subCategory);
+			
+			Thread.sleep(2000);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(xpath)).click();
+			String bool_expand = driver.findElement(xpath).getAttribute("aria-expanded");
+			Assert.assertEquals("Test Fail: Sub Category \""+ subCategory + "\" does not expand", "true", bool_expand);
+			System.out.println("Test Pass: Sub Category \"" + subCategory + "\" expanded");
+			if ( i == 0) {
+				String s1="MTN";
+				String s2 = hTable.get("MTN");
+				Thread.sleep(2000);
+				List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"accordion__panel-4\"]/div"));
+				for (WebElement element : elements) {
+					s1 = s1 + "," + element.findElement(By.tagName("span")).getText();
+					s2 = s2 + "," + element.findElement(By.tagName("p")).getText();
+				}
+				System.out.println("S1 : " + s1);
+				System.out.println("S2 : " + s2);
+				Datattest.createHeader(s1);
+				Datattest.updateResultsSummary2(s2);
+			}
+		}	
+		
+		//Expand Network and customer activity
+		wait.until(ExpectedConditions.visibilityOfElementLocated(NetworkAndCustomerActivity)).click();
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(ExpandedNetworkAndCustomerActivity));
+			System.out.println("Test Pass: Network And Customer Activity Category expanded");
+		}catch(Exception e) {
+			Assert.fail("Test Fail: Network And Customer Activity Category does not expand");
+		}
+		
+		//Expand Third Party Insights
+		wait.until(ExpectedConditions.visibilityOfElementLocated(ThirdPartyInsights)).click();
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(ExpandedThirdPartyInsights));
+			System.out.println("Test Pass: Third Party Insights Category expanded");
+		}catch(Exception e) {
+			Assert.fail("Test Fail: Third Party Insights Category does not expand");
+		}
+		
+		//Expand Add-Ons
+		wait.until(ExpectedConditions.visibilityOfElementLocated(AddOns)).click();
+		try {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(ExpandedAddOns));
+			System.out.println("Test Pass: Add-Ons Category expanded");
+		}catch(Exception e) {
+			Assert.fail("Test Fail: Add-Ons Category does not expand");
+		}
+		
+		//Assert Sub-category titles of Add-Ons
+		for (int i=0; i<subCategoryAddOns.size();i++) {
+			String subCategory = subCategoryAddOns.get(i);
+			By xpath = SubCategoryXpath(subCategory);
+			try {
+				wait.until(ExpectedConditions.visibilityOfElementLocated(xpath));
+				System.out.println("Test Pass: Sub Category \"" + subCategory + "\" is available");
+			}catch (Exception e) {
+				Assert.fail("Test Fail: Sub Category \""+ subCategory + "\" is not available");
+			}
+		}
+		
+		//Expand Sub-category of Add-Ons
+		for (int i=0; i<subCategoryAddOns.size();i++) {
+			String subCategory = subCategoryAddOns.get(i);
+			By xpath = SubCategoryXpath(subCategory);
+			
+			wait.until(ExpectedConditions.visibilityOfElementLocated(xpath)).click();
+			String bool_expand = driver.findElement(xpath).getAttribute("aria-expanded");
+			Assert.assertEquals("Test Fail: Sub Category \""+ subCategory + "\" does not expand", "true", bool_expand);
+			System.out.println("Test Pass: Sub Category \"" + subCategory + "\" expanded");
+		}
 	}
 }
-
-
-			//System.out.println(str);
-
 
 
 
